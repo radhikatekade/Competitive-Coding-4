58fc16cb7dfe7b3753270c1baa172d6ab17858e2
Radhika Tekade <tekade.radhika@gmail.com>
2025-02-03 14:59:23 -0800

    Create 80_height_balanced_BT

---
+++ 80_height_balanced_BT.py
@@ -0,0 +1,26 @@
+# Time complexity - O(n)
+# Space complexity - O(h)
+
+# Approach - Have another function that computes height of the subtree, compare height of subtree.left
+# to subtree.right, return False if diff > 1, finally if False condition not found, return True.
+
+from typing import Optional
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if root is None:
+            return True
+        if abs(self.height(root.left) - self.height(root.right)) > 1:
+            return False
+        return self.isBalanced(root.left) and self.isBalanced(root.right)
+    
+    def height(self, root: TreeNode) -> int:
+        if not root:
+            return -1
+        return 1 + max(self.height(root.left), self.height(root.right))
